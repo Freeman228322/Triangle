0b7d4e2b2a1bc4bc286d235de5a0ea1a455840e9
Alex Freeman <alexstoun228@gmail.com>
2025-07-08 17:44:17 +0300

    Update Program.cs

--- Man/Program.cs
+++ Man/Program.cs
@@ -1,4 +1,4 @@
-﻿global using static System.Console;
+global using static System.Console;
 using System;
 
 namespace Man
@@ -66,11 +66,31 @@ namespace Man
         }
         public Triangle(double first_side, double second_side, double third_side, double height)
         {
+            // Проверка на положительные стороны
+            if (first_side <= 0 || second_side <= 0 || third_side <= 0)
+                throw new ArgumentException("Все стороны должны быть положительными");
+
+            // Сначала устанавливаем значения сторон
             First_side = first_side;
             Second_side = second_side;
             Third_side = third_side;
-            Height = height;
 
+            // Проверка существования треугольника
+            if (first_side + second_side <= third_side ||
+                first_side + third_side <= second_side ||
+                second_side + third_side <= first_side)
+            {
+                throw new ArgumentException("Треугольник с такими сторонами не существует");
+            }
+
+            // Проверка высоты 
+            double max_possible_height = 2 * CalculateArea() / GetOsnovanie();
+            if (height <= 0 || height > max_possible_height)
+            {
+                throw new ArgumentException($"Некорректная высота. Допустимый диапазон: 0 < h ≤ {max_possible_height:F2}");
+            }
+
+            Height = height;
         }
         public double GetPerim() 
         {
@@ -85,6 +105,11 @@ namespace Man
         {
             return (Height * GetOsnovanie() / 2);
         }
+        private double CalculateArea()
+        {
+            double p = GetPerim() / 2;
+            return Math.Sqrt(p * (p - First_side) * (p - Second_side) * (p - Third_side));
+        }
 
     }
 
@@ -92,7 +117,7 @@ namespace Man
     {
         static void Main(string[] args)
         {
-            Triangle triangle = new Triangle(5, 6, 12, 2);
+            Triangle triangle = new Triangle(7, 6, 12, 2);
 
             double number1 = triangle.GetPerim();
             double number2 = triangle.GetPloshad();
@@ -100,7 +125,7 @@ namespace Man
             Console.WriteLine($"Основание {triangle.GetOsnovanie()} и высота {triangle.Height}. Площадь равна {number2}");
             WriteLine("asdasd");
 
-
+            Console.WriteLine("");
 
 
 
